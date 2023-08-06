eca7f239bb3f63c2705fc3c8b471d6a51875dc86
BayKeremm <kerem13okyay@gmail.com>
2023-08-06 09:25:50 +0200

    added wgrep

--- OSTEP/projects/initial-utilities/wgrep.c
+++ OSTEP/projects/initial-utilities/wgrep.c
@@ -1,22 +1,48 @@
 #include <stdio.h>
 #include <stdlib.h>
-int grepFunc(char * fileName){
-    FILE * file = fopen(fileName,"r");
-    if(file == NULL){
-        printf("wgrep: cannot open file\n");
+#include <string.h>
+int grepFunc(FILE * file, char * toSearch){
+    char * linep;
+    size_t n = 100;
+
+    linep = (char *) malloc(n * sizeof(char));
+    if (linep == NULL){
+        printf("Unable to allocate buffer\n");
         return 1;
     }
-    // TODO: grep utility
-    
-    fclose(file);
+    while (getline(&linep,&n,file) > -1){
+        if(strstr(linep,toSearch) != NULL){
+            printf("%s",linep);
+        }
+    }
+    free(linep);
     return 0;
 }
 int main(int argc, char * argv[]){
-    if (argc < 3){
-        printf("Error, usage is wrong\n");
-        return 0;
+    if (argc < 2){
+        printf("wgrep: searchterm [file ...]\n");
+        exit(1);
     }
-    for(int i = argc -1; i > 0 ; i--){
+    char * s = argv[1];
+    if (argc == 2){
+        char res = grepFunc(stdin,s);
+        if(res){
+            exit(1);
+        }
+    }else{
+        for(int i = argc - 1 ; i > 1 ; i--){
+            FILE * file = fopen(argv[i],"r");
+            if(file == NULL){
+                printf("wcat: cannot open file\n");
+                exit(1);
+            }
+            char res = grepFunc(file,s);
+            fclose(file);
+            if(res){
+                exit(1);
+            }
+        }
+
     }
     return 0;
 }
